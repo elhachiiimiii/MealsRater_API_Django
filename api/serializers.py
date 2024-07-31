bc62e669bc902b9e1e97fd4d34aabad6e936e4be
elhachiiimiii <abdellahhachimi20@gmail.com>
2024-07-31 19:47:00 +0200

    add avg , no of ratings with test http://127.0.0.1:8000/api/meals/pk_meal/rate_meal

--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,7 @@ from .models import Meal, Rating
 class MealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meal
-        fields = ('id', 'title', 'description',)
+        fields = ('id', 'title', 'description', 'avg_rating')
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
